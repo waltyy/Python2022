286f4a09a64dcfb87b65b6b77210bd9fb38efb0a
waltyy <100096985+waltyy@users.noreply.github.com>
2022-02-24 14:29:51 +1300

    Update 2_data_types.py

--- 2_data_types.py
+++ 2_data_types.py
@@ -1,8 +1,62 @@
 #Print the chorus from your favourite song in a single print statement, using line breaks escapes.
+print("Fortnite battle pass\nI just sh*t out my ass")
 #Print a quote from a movie using escape characters.
+print("I\nAm\nIron Man")
 #Print out your definition of what a string is
+print("A string in python is a text being printed out as it is")
 #Print out your definition of what an integer is
+print("An integer in python is numbers/maths")
 #Print out your definition of what a float is
+print("A float in python is numbers with decemals")
 #Use division to find the answer to 543 divided by 3
+print(543/3)
 #Use integer division to find the answer to 543 divided by 3
+print(543//3)
 #Use modulo to find the remainder of  the 543 divided by 3
+print(543%3)
+#1000 modulo 9
+print(1000%9)
+#434 integer division 10
+print(434//10)
+#3 modulo 2
+print(3%2)
+#5 modulo 5
+print(5%5)
+#555 divided by 2
+print(555/2)
+#123/2
+print(123/2)
+#123//2
+print(123//2)
+#123%2
+print(123%2)
+#764%2
+print(764%2)
+#165//2
+print(165//2)
+#Add two floats together
+print(10.5+11.7)
+#Add two integers together
+print(80+15)
+#Subtract a float from an integer
+print(10.9-3)
+#Subtract an integer from a float
+print(5-2.5)
+#Divide an integer by another integer (not zero, this will cause an error)
+print(6/3)
+#Multiply an integer by a float
+print(9*7.48532)
+#Multiply an integer by an integer
+print(4.6748732647238364*7.464329823983455)
+#Print your name out 10 times
+print("ashane"*10)
+#Print your name out 10 times, each on a new line (use escape characters)
+print("ashane\n"*10)
+#Use type conversion to print out the following sentence – 5+10=15
+print("5+10=" + str(5+10))
+#Use type conversion to print out the following sentence 15/2=7.5
+print("15/2=" + str(15/2))
+#Use type conversion to print out the following sentence 30/2*5=75.0
+print("30/2*5=" + str(30/2*5))
+#Use type conversion to print out the following sentence 30/2*5=75 (note the subtle difference here)
+print("30/2*5=" + str(int(30/2*5)))
