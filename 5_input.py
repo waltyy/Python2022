eca8415d358e30a3314a61f41f3542585dcf990c
waltyy <100096985+waltyy@users.noreply.github.com>
2022-03-01 10:56:10 +1300

    Done some of exercise 5

---
+++ 5_input.py
@@ -0,0 +1,22 @@
+#Write code that asks the user to simply push any key, followed by enter
+user_key = input("Simply push any key, then press enter\n")
+print("You pressed", user_key)
+#Ask the user to type their name and push enter
+user_name = input("Please type your name\n")
+print("Hello", user_name)
+#Ask the user to type their age and push enter
+user_age = input("How old are you?\n")
+print("You are", user_age,"years old")
+#Ask the user to type their name and store the response in a variable. Ask the user to type their age and store the response in a variable
+print("You are", user_name,"and you are", user_age,"years old")
+#Ask the user for their favourite movie and store it in a variable
+user_movie = input("What is your favourite movie?\n")
+#Ask the user for the name of a book and store it in a variable
+user_book = input("What is the name of a book?\n")
+#Ask the user for an adjective and store it in a variable
+user_adjective = input("Name an adjective\n")
+#Ask the user for a noun and store it in a variable
+user_noun = input("Name a noun\n")
+#Ask the user for a verb and store it in a variable
+user_verb = input("Name a verb\n")
+#Create 8 different print statements using one of the methods above to insert the variables into each string. Use multiple variables in each string.4
