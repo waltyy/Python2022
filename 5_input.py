fb67089e79a2b5079b2b35d5594fa939255dc7e7
waltyy <100096985+waltyy@users.noreply.github.com>
2022-03-02 12:20:12 +1300

    Did some of exercise 5

--- 5_input.py
+++ 5_input.py
@@ -19,4 +19,12 @@ user_adjective = input("Name an adjective\n")
 user_noun = input("Name a noun\n")
 #Ask the user for a verb and store it in a variable
 user_verb = input("Name a verb\n")
-#Create 8 different print statements using one of the methods above to insert the variables into each string. Use multiple variables in each string.4
+#Create 8 different print statements using one of the methods above to insert the variables into each string. Use multiple variables in each string.
+#Ask the user for their age and store it in a variable
+#Print out how old the user will be in 10 years
+user_age = user_age + 10
+print("You will be", user_age,"in 10 years")
+#Print out the year that the user was born in (approximately, no need to consider months)
+#Ask the user how many apples they have and store it in a variable
+#Ask the user how many friends they have and store it in a variable
+#Print out how many apples they can share equally amongst their friends, and how many apples they have left over (hint : use modulo and int division)python 5_input.py
